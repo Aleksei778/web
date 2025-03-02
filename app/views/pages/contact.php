4e77852ae0283d1ab444234fb4e70622c24ae70c
Aleksei778 <aleksejparhomenko14192@gmail.com>
2025-03-02 10:51:12 +0300

    123

--- app/views/pages/contact.php
+++ app/views/pages/contact.php
@@ -4,7 +4,7 @@
     </h2>
     <div class="info-container">
         <div class="contact-form">
-            <form action="/laba8/LABA8/main/validateContact" method="POST" id="contactForm" style="position: sticky; z-index: 1000;">
+            <form action="/web/main/validateContact" method="POST" id="contactForm" style="position: sticky; z-index: 1000;">
                 <!-- ФИО -->
                 <label for="fullName">ФИО:</label>
                 <input type="text" name="fullName" id="fullName" placeholder="Введите ФИО" class="contact_form-input" data-format="Пархоменко Алексей Александрович">
